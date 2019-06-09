08a7996745c27f69d9af375eb38aac30eb950def
devansien <devansien@gmail.com>
2019-06-09 18:41:31 +1200

    batch update

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from nltk import ne_chunk, pos_tag, word_tokenize
 
 
 def get_continuous_chunks(text):
+    sentence_count = 1
     chunked = ne_chunk(pos_tag(word_tokenize(text)))
-    prev = None
     continuous_chunk = []
     current_chunk = []
-    #print(chunked)
+    # prev = None
+
     for i in chunked:
+        if str(i[0]) == '.':
+            sentence_count += 1
         if type(i) == Tree:
-            current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+            current_chunk.append(str(sentence_count) + " " + str([token for token, pos in i.leaves()]))
         elif current_chunk:
             named_entity = " ".join(current_chunk)
             if named_entity not in continuous_chunk:
@@ -28,10 +31,14 @@ def get_continuous_chunks(text):
     return continuous_chunk
 
 
-txt = "Jacinda Ardern is the Prime Minister Bryan of New Zealand but Roenzo isn't."
+# load text
+with open('data/Firearms.txt', 'r') as file:
+    txt = file.read().replace('\n', ' ')
+
+# tag, label, and sentence numbers
 print(get_continuous_chunks(txt))
 
-for sent in nltk.sent_tokenize(txt):
-    for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-        if hasattr(chunk, 'label'):
-            print(chunk.label(), ' '.join(c[0] for c in chunk))
+# for sent in nltk.sent_tokenize(txt):
+#     for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+#         if hasattr(chunk, 'label'):
+#             print(chunk.label(), ' '.join(c[0] for c in chunk))
